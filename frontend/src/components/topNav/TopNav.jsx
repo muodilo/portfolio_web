8c9480f6a6381e63a1f491805ecbfdb2e8bb4a7f
odilo <odilomurindahabi@gmail.com>
2024-01-30 01:44:35 +0200

    install react-router-dom

--- frontend/src/components/topNav/TopNav.jsx
+++ frontend/src/components/topNav/TopNav.jsx
@@ -4,8 +4,20 @@ const TopNav = () => {
   return (
     <div className="px-0 md:px-[60px] lg:px-[100px] h-[70px]">
       <div>
-        <div className="brand">
+        {/* brand */}
+
+        <div className="">
           Odilo
+        </div>
+
+        {/* nav Links */}
+        <div>
+
+        </div>
+
+        {/* book a call button */}
+        <div>
+
         </div>
       </div>
     </div>
