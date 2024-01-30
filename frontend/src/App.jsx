92b4148fe186388e64fd1095107ae41bf0fb5fc6
odilo <odilomurindahabi@gmail.com>
2024-01-30 02:02:47 +0200

    add navLinks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
+import { BrowserRouter as Router,Routes,Route } from "react-router-dom"
 import TopNav from "./components/topNav/TopNav"
 
 function App() {
 
   return (
-    <>
-      <TopNav/>
-    </>
+    <Router>
+      <TopNav />
+
+      <Routes>
+        <Route/>
+      </Routes>
+
+    </Router>
   )
 }
 
--- frontend/src/components/topNav/TopNav.jsx
+++ frontend/src/components/topNav/TopNav.jsx
@@ -1,21 +1,42 @@
-
+import { Link } from "react-router-dom"
 
 const TopNav = () => {
   return (
-    <div className="px-0 md:px-[60px] lg:px-[100px] h-[70px]">
-      <div>
+    <div className="px-0 md:px-[60px] lg:px-[100px] ">
+      <div className="flex items-center min-h-[70px] justify-between w-full">
         {/* brand */}
 
         <div className="">
-          Odilo
+          <Link to='/' className="text-2xl">ODILO</Link>
         </div>
 
         {/* nav Links */}
+        <ul className="flex">
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+          <li>
+            <Link to='/projects'>Projects</Link>
+          </li>
+          <li>
+            <Link to='/blog'>Blog</Link>
+          </li>
+          <li>
+            <Link to='/contact'>Contact</Link>
+          </li>
+          <li>
+            <Link to='/services'>Services</Link>
+          </li>
+        </ul>
+
+        {/* book a call button */}
         <div>
 
         </div>
-
-        {/* book a call button */}
+        {/* menu icon */}
         <div>
 
         </div>
