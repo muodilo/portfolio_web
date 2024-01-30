3dbf0d8c89f8785db985722757b1018853faec51
odilo <odilomurindahabi@gmail.com>
2024-01-30 02:54:11 +0200

    make navbar responsive

--- frontend/src/components/topNav/TopNav.jsx
+++ frontend/src/components/topNav/TopNav.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom"
+import { RiMenu5Line } from "react-icons/ri";
 
 const TopNav = () => {
   return (
     <div className="px-0 md:px-[60px] lg:px-[100px] shadow">
-      <div className="flex items-center min-h-[70px] justify-between w-full ">
+      <div className="flex items-center min-h-[70px] justify-between w-full px-[10px] md:px-0">
         {/* brand */}
 
         <div className="">
@@ -38,10 +39,26 @@ const TopNav = () => {
         </div>
         {/* menu icon */}
         <div className="dropdown dropdown-bottom dropdown-end md:hidden block">
-          <div tabIndex={0} role="button" className="btn m-1">C</div>
+          <div tabIndex={0} role="button" className="btn m-1"><RiMenu5Line /></div>
           <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>Item 1</a></li>
-            <li><a>Item 2</a></li>
+          <li className='me-5 font-bold hover:font-bold transition'>
+            <Link to='/'>Home</Link>
+          </li>
+          <li className='me-5  hover:font-bold transition'>
+            <Link to='/about'>About</Link>
+          </li>
+          <li className='pe-5  hover:font-bold transition'>
+            <Link to='/projects'>Projects</Link>
+          </li>
+          <li className='me-5  hover:font-bold transition'>
+            <Link to='/blog'>Blog</Link>
+          </li>
+          <li className='me-5  hover:font-bold transition'>
+            <Link to='/contact'>Contact</Link>
+          </li>
+          <li className='me-5  hover:font-bold transition'>
+            <Link to='/services'>Services</Link>
+          </li>
           </ul>
         </div>
       </div>
