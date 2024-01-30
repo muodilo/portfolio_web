604b801b5297a6e9aaae602211314bac134fb412
odilo <odilomurindahabi@gmail.com>
2024-01-30 02:40:57 +0200

    install react-icons

--- frontend/src/components/topNav/TopNav.jsx
+++ frontend/src/components/topNav/TopNav.jsx
@@ -11,34 +11,38 @@ const TopNav = () => {
         </div>
 
         {/* nav Links */}
-        <ul className="flex">
-          <li className='pe-5 font-bold'>
+        <ul className="md:flex hidden">
+          <li className='me-5 font-bold hover:font-bold transition'>
             <Link to='/'>Home</Link>
           </li>
-          <li className='pe-5 font-bold'>
+          <li className='me-5  hover:font-bold transition'>
             <Link to='/about'>About</Link>
           </li>
-          <li className='pe-5 font-bold'>
+          <li className='pe-5  hover:font-bold transition'>
             <Link to='/projects'>Projects</Link>
           </li>
-          <li className='pe-5 font-bold'>
+          <li className='me-5  hover:font-bold transition'>
             <Link to='/blog'>Blog</Link>
           </li>
-          <li className='pe-5 font-bold'>
+          <li className='me-5  hover:font-bold transition'>
             <Link to='/contact'>Contact</Link>
           </li>
-          <li className='pe-5 font-bold'>
+          <li className='me-5  hover:font-bold transition'>
             <Link to='/services'>Services</Link>
           </li>
         </ul>
 
         {/* book a call button */}
-        <div>
-
+        <div className="ms-auto md:ms-0">
+          <button className="btn">Book a call</button>
         </div>
         {/* menu icon */}
-        <div>
-
+        <div className="dropdown dropdown-bottom dropdown-end md:hidden block">
+          <div tabIndex={0} role="button" className="btn m-1">C</div>
+          <ul tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+            <li><a>Item 1</a></li>
+            <li><a>Item 2</a></li>
+          </ul>
         </div>
       </div>
     </div>
