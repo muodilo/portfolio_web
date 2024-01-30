59b8cc6774bc8f80f1dd4c43cd89df83920eea10
odilo <odilomurindahabi@gmail.com>
2024-01-30 02:12:37 +0200

    import ropoto google font

--- frontend/src/components/topNav/TopNav.jsx
+++ frontend/src/components/topNav/TopNav.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom"
 
 const TopNav = () => {
   return (
-    <div className="px-0 md:px-[60px] lg:px-[100px] ">
-      <div className="flex items-center min-h-[70px] justify-between w-full">
+    <div className="px-0 md:px-[60px] lg:px-[100px] shadow">
+      <div className="flex items-center min-h-[70px] justify-between w-full ">
         {/* brand */}
 
         <div className="">
@@ -12,22 +12,22 @@ const TopNav = () => {
 
         {/* nav Links */}
         <ul className="flex">
-          <li>
+          <li className='pe-5 font-bold'>
             <Link to='/'>Home</Link>
           </li>
-          <li>
+          <li className='pe-5 font-bold'>
             <Link to='/about'>About</Link>
           </li>
-          <li>
+          <li className='pe-5 font-bold'>
             <Link to='/projects'>Projects</Link>
           </li>
-          <li>
+          <li className='pe-5 font-bold'>
             <Link to='/blog'>Blog</Link>
           </li>
-          <li>
+          <li className='pe-5 font-bold'>
             <Link to='/contact'>Contact</Link>
           </li>
-          <li>
+          <li className='pe-5 font-bold'>
             <Link to='/services'>Services</Link>
           </li>
         </ul>
