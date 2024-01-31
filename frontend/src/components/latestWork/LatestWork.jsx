7453afb9b6e23627563bb5ca30c351b98c1f3061
odilo <odilomurindahabi@gmail.com>
2024-01-31 19:41:17 +0200

    add rotation on header image when is whovered

---
+++ frontend/src/components/latestWork/LatestWork.jsx
@@ -0,0 +1,11 @@
+
+
+const LatestWork = () => {
+  return (
+    <div>
+      Check out my latest work
+    </div>
+  )
+}
+
+export default LatestWork
--- frontend/src/pages/layout/Layout.jsx
+++ frontend/src/pages/layout/Layout.jsx
@@ -1,11 +1,13 @@
 import Header from "../../components/header/Header"
 import Services from "../../components/services/Services"
+import LatestWork from "../../components/latestWork/LatestWork"
 
 const Layout = () => {
   return (
     <div>
       <Header />
       <Services />
+      <LatestWork/>
     </div>
   )
 }
