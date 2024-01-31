ab319d6828d5580d2a43af712d83e9960b3d9e02
odilo <odilomurindahabi@gmail.com>
2024-01-31 19:43:22 +0200

    change the oppacity of header text

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
         {/* left side of header */}
         <div className="lg:col-span-3 md:col-span-1 p-4 flex flex-col  justify-center animate-in slide-in-from-left duration-300">
           <h1 className="lg:text-[50px] md:text-[40px] text-[30px] font-bold mb-5">Full Stack Alchemist: Weaving Magic with Code</h1>
-          <p className="text-xl text-gray-500 mb-[30px]">Hello, I'm Odilo Murindahabi, a dedicated Full Stack Developer with a knack for turning ideas into captivating digital experiences. Through a blend of frontend finesse and backend wizardry, I bring simplicity and elegance to complex projects. Let's collaborate and bring your vision to life through the art of code.</p>
+          <p className="text-xl text-gray-700 mb-[30px]">Hello, I'm Odilo Murindahabi, a dedicated Full Stack Developer with a knack for turning ideas into captivating digital experiences. Through a blend of frontend finesse and backend wizardry, I bring simplicity and elegance to complex projects. Let's collaborate and bring your vision to life through the art of code.</p>
           <div>
             <button className="btn btn-accent z-30">Book a call with me</button>
 
