31930440ec8355dc8e3e34109f5699534564bfa1
odilo <odilomurindahabi@gmail.com>
2024-02-02 23:53:03 +0200

    create testimonial courosel using flowbite-react

--- frontend/src/components/testimonials/Testimonials.jsx
+++ frontend/src/components/testimonials/Testimonials.jsx
@@ -5,6 +5,7 @@ import { Carousel } from 'flowbite-react';
 
 function Testimanials() {
   return (
+    <section className='py-[70px] px-[300px]'>
     <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
       <Carousel>
         <div className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
@@ -17,7 +18,8 @@ function Testimanials() {
           Slide 3
         </div>
       </Carousel>
-    </div>
+      </div>
+    </section>
   );
 }
 
