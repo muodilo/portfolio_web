1b8aa047cd8e1201efe0b0e7f4759d3eb85ac43e
odilo <odilomurindahabi@gmail.com>
2024-02-07 09:39:45 +0200

    remove .env file

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,10 @@ const app = express();
 app.get('/', (req, res) => {
   res.json({ message: 'hello' });
 })
+app.post('/', (req, res) => {
+  res.json({ message: 'hello' });
+})
+
 
 
 app.listen(5000, () => console.log("server is listening on port 5000"));
