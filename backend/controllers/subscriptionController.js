d60a59336b0355bb3d866375339c8bfe74eda8c4
odilo <odilomurindahabi@gmail.com>
2024-02-08 07:40:21 +0200

    check if email already subscribed

--- backend/controllers/subscriptionController.js
+++ backend/controllers/subscriptionController.js
@@ -1,1 +1,24 @@
 const asyncHandler = require('express-async-handler')
+const Subscription = require('../models/Subscription.js');
+
+//subscribe to a newslatter
+
+export const subscribe = asynchandler(async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    res.status(403);
+    throw new Error('Please provide an email');
+  }
+
+  try {
+    //check if allready subscribed
+    const subscribedEmail = await Subscription({ email });
+    if (subscribedEmail) {
+      res.status(403);
+      throw new Error('Already subscribed');
+    }
+  } catch (error) {
+    
+  }
+})
