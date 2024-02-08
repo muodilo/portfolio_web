4a59263da0128e11b92d521e0f3e0ab4fc0f0ff1
odilo <odilomurindahabi@gmail.com>
2024-02-08 09:28:00 +0200

    create post Model

---
+++ backend/models/postModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const postSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required:true,
+  },
+  content: {
+    type: String,
+    required:true,
+  },
+  image: {
+    type:String,
+  },
+  category: {
+    type: String,
+    enum:[Business,Software_development, Tutorials,Technology]
+  }
+}, {
+  timestamps:true,
+})
