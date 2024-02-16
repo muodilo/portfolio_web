1085e2860a91cdfe1c8037478f585eb08c2639ff
odilo <odilomurindahabi@gmail.com>
2024-02-16 20:05:22 +0200

    configure redux in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './app/store';
 import App from './App.jsx'
 import './index.css'
+import { PersistGate } from 'redux-persist/integration/react'; 
+import { persistStore } from 'redux-persist';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+let persistor = persistStore(store);
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
 )
