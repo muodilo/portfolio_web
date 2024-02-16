1e4cc8d671332fc90d53104ac8c418189d4908df
odilo <odilomurindahabi@gmail.com>
2024-02-16 19:21:40 +0200

    configure storePersistReducer

---
+++ frontend/src/app/store.js
@@ -0,0 +1,21 @@
+import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+import { combineReducers } from '@reduxjs/toolkit';
+
+const persistConfig = {
+  key: 'root',
+  version: '1',
+  storage
+}
+const rootReducer = combineReducers({
+
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: {
+    reducer:persistedReducer
+  }
+})
