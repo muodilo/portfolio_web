708a20b486b254c6f1dc7fb0c4cb0f600c1a4f22
odilo <odilomurindahabi@gmail.com>
2024-02-22 23:13:05 +0200

    create getPostById route

--- backend/routes/postsRoutes.js
+++ backend/routes/postsRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createPost,getAllPosts,getCurrentThreePosts } = require('../controllers/postController.js');
+const { createPost,getAllPosts,getCurrentThreePosts,getPostById } = require('../controllers/postController.js');
 
 const router = express.Router();
 
 router.post('/', createPost);
 router.get('/', getAllPosts);
 router.get('/currentThreePosts', getCurrentThreePosts);
+router.get('/:id', getPostById);
 
 module.exports = router;
