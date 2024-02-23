36d99ab3102cbd9e71302d96b820fb3c252b1664
odilo <odilomurindahabi@gmail.com>
2024-02-23 22:17:05 +0200

    create relatedPost card

---
+++ frontend/src/pages/singlePost/RelatedPostCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const RelatedPostCard = () => {
+  return (
+    <div className="   flex flex-col items-start cursor-pointer shadow-xl p-2 rounded bg-slate-50">
+          
+    <h2 className="sm:text-2xl text-xl title-font font-medium text-gray-900 mt-4 mb-4">Roof party normcore before they sold out, cornhole vape</h2>
+    <p className="leading-relaxed">Live-edge letterpress cliche, salvia fanny pack humblebrag narwhal portland. </p>
+
+    </div>
+            
+  )
+}
+
+export default RelatedPostCard
--- frontend/src/pages/singlePost/SinglePost.jsx
+++ frontend/src/pages/singlePost/SinglePost.jsx
@@ -46,13 +46,7 @@ const SinglePost = () => {
             <hr />
           
             
-            <div className="   flex flex-col items-start cursor-pointer shadow-xl p-2 rounded bg-slate-50">
-          
-                <h2 className="sm:text-2xl text-xl title-font font-medium text-gray-900 mt-4 mb-4">Roof party normcore before they sold out, cornhole vape</h2>
-                <p className="leading-relaxed">Live-edge letterpress cliche, salvia fanny pack humblebrag narwhal portland. </p>
-
-            </div>
-            <hr />
+            
           
             
 
