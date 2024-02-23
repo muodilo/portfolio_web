3a08de81765fe379a31d89acd29a0ce0a5687d8f
odilo <odilomurindahabi@gmail.com>
2024-02-23 21:53:28 +0200

    create getRelatePost service

--- frontend/src/features/post/postService.js
+++ frontend/src/features/post/postService.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const GET_CURRENT_THREE_POSTS = 'http://localhost:5000/api/v1/posts/currentThreePosts';
-const GET_ALL_POSTS='http://localhost:5000/api/v1/posts'
+const GET_ALL_POSTS = 'http://localhost:5000/api/v1/posts';
+const GET_RELATED_POSTS = 'http://localhost:5000/api/v1/posts/related'
+
 
 const getCurrentThreePosts = async () => {
   const response = await axios.get(GET_CURRENT_THREE_POSTS);
@@ -18,10 +20,16 @@ const getSpecificPost = async (id) => {
   return response.data;
 }
 
+const getRelatedPosts = async (category) => {
+  const response = await axios.get(`${GET_RELATED_POSTS}/${category}`);
+  return response.data;
+}
+
 const postServices = {
   getCurrentThreePosts,
   getAllPosts,
   getSpecificPost,
+  getRelatedPosts
 }
 
 export default postServices;
