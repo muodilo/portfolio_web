c91ea7993f4fbda9705a1baba431c497a6ea3b85
odilo <odilomurindahabi@gmail.com>
2024-02-24 08:31:50 +0200

    render related posts dynamically

--- frontend/src/pages/singlePost/RelatedPostCard.jsx
+++ frontend/src/pages/singlePost/RelatedPostCard.jsx
@@ -1,6 +1,27 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux';
+import { getSpecificPost, reset } from '../../features/post/postSlice';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+
 
 const RelatedPostCard = ({ post }) => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    const fetchSinglePost = async () => {
+      try {
+        await dispatch(getSpecificPost(post._id));
+        navigate('/blog/' + post._id);
+        dispatch(reset());
+      } catch (error) {
+        toast.error(isSinglePostErrorMessage)
+      }
+    }
+    fetchSinglePost();
+    dispatch(reset());
+  }
 
   const truncateContent = (content, wordCount = 10) => {
     const words = content.split(/\s+/); // Split by spaces
@@ -9,7 +30,7 @@ const RelatedPostCard = ({ post }) => {
     return truncatedContent;
   };
   return (
-    <div className="   flex flex-col items-start cursor-pointer shadow-xl p-2 rounded bg-slate-50 mb-4">
+    <div className="   flex flex-col items-start cursor-pointer shadow-xl p-2 rounded bg-slate-50 mb-4" onClick={handleClick}>
           
       <h2 className="sm:text-2xl text-xl title-font font-medium text-gray-900 mt-4 mb-4">{post.title }</h2>
     <p className="leading-relaxed">{truncateContent(post.content, 10)}</p>
