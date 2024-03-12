1401e32d855832e3189ed05bb0178e8ae2b576b0
odilo <odilomurindahabi@gmail.com>
2024-03-12 10:39:25 +0200

    create login functionality

--- backend/controllers/userControler.js
+++ backend/controllers/userControler.js
@@ -41,6 +41,42 @@ const createUser = async(async (req, res) => {
   }
 })
 
+//login
+
+const loginUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(401)
+    throw new Error('Please provide both email and password');
+  }
+
+  try {
+    //check if user with provided email exists
+    const user = await User.findOne({ email });
+    if (!user) {
+      res.status(401)
+      throw new Error('Invalid email or Password');
+    }
+
+    //ckeck if the provided password matches the stored hashed password
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+      res.status(401)
+      throw new Error('Invalid password');
+    }
+    res.status(200).json({
+      id: user._id,
+      username: user.username,
+      email: user.email,
+      role: user.role,
+      token: generateToken(user._id)
+    })
+  } catch (error) {
+    res.status(500)
+    throw new Error(error.message);
+  }
+})
+
 //generate token function
 const generateToken = (id) => {
   const token = jwt.sign(
