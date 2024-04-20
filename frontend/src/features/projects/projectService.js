ac4b51102af748dff0eade3c0238f19529c2b91c
odilo <odilomurindahabi@gmail.com>
2024-04-20 11:37:42 +0200

    create delete Project service

--- frontend/src/features/projects/projectService.js
+++ frontend/src/features/projects/projectService.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 const GET_CURRENT_THREE_PROJECTS = 'http://localhost:5000/api/v1/projects/currentThreeProjects';
 const GET_ALL_PROJECTS = 'http://localhost:5000/api/v1/projects';
 const CREATE_PROJECT_API_URL = 'http://localhost:5000/api/v1/projects';
+const DELETE_PROJECT_API_URL = 'http://localhost:5000/api/v1/projects'
 
 
 const getCurrentThreeProjects = async () => {
@@ -27,10 +28,27 @@ const createProject = async (formData, token) => {
   return response.data;
 }
 
+const deleteProject = async (projectId, token) => {
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    const response = await axios.delete(`${DELETE_PROJECT_API_URL}/${projectId}`, config);
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.data.message || 'Failed to delete project');
+  }
+};
+
+
 const projectServices = {
   getCurrentThreeProjects,
   getAllProjects,
   createProject,
+  deleteProject,
 }
 
 export default projectServices;
